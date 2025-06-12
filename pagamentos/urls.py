d6a320ead49486cd6fb2f7d1b3ac6c03a38993a2
renanppcosta <renanppcosta@gmail.com>
2025-06-11 23:04:24 -0300

    Feat APIs de Cartão e pix implementadas! Stripe e AbacatePay

--- pagamentos/urls.py
+++ pagamentos/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from pagamentos.views import (
-    CheckoutView
+    CheckoutView,
+    create_payment,
+    abacate_pay_webhook
 )
 
 app_name = 'pagamentos'
 
 urlpatterns = [
     path('checkout/', CheckoutView.as_view(), name='pagamentos'),
+    path("create-payment-intent/", create_payment, name="create_payment"),
+    path("/webhook/abacatepay/", abacate_pay_webhook, name="abacate_pay_webhook")
 ]
 
